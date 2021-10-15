b3ea4f36f594343a2417df4d74776039377fda3b
Karan <karanluthria@gmail.com>
2021-10-15 02:25:29 -0400

    typeformapi

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const Discord = require("discord.js");
 const fs = require("fs");
+const { createClient } = require('@typeform/api-client')
 
 const IDList = require("./ids.json");
 const names = require("./names.json");
@@ -13,17 +14,71 @@ const settings = {
     OWNER_ID: "200042113814102016"
 };
 
+//var main_registration1 = {}
+var main_registration2 = {}
+
+var main_registrations = []
+
+async function getAPIResponses(){
+  all_responses = {}
+  var typeformAPI = createClient({ token: 'FSMBuwxfPazBiMupZuY6gHHtnCtM44qEEAVcdybveqE3' })
+    var  main_registration1 = await typeformAPI.responses.list({
+              uid:'Bcr4BHNV',
+              pageSize:1000});
+    all_responses["main_registration1"] = await typeformAPI.responses.list({
+          uid:'Bcr4BHNV',
+          pageSize:1000});
+    var all_emails_schools = []
+    for (const x of all_responses["main_registration1"]["items"]){
+      if (typeof(x["answers"][20]) != "undefined"  && typeof(x["answers"][20]["text"]) != "undefined"){
+        all_emails_schools.push([x["answers"][2]["email"], x["answers"][5]["text"], x["answers"][20]["text"]])
+      }else{
+        all_emails_schools.push([x["answers"][2]["email"], x["answers"][5]["text"], ""])
+
+      }
+    }
+
+    all_responses["main_registration2"] = await typeformAPI.responses.list({
+          uid:'Bcr4BHNV',
+          pageSize:1000,
+          before: main_registration1["items"][main_registration1["items"].length - 1]["token"]
+
+      });
+    for (const x of all_responses["main_registration2"]["items"]){
+
+        all_emails_schools.push([x["answers"][2]["email"], x["answers"][5]["text"], x["answers"][20]["text"]])
+      }
+    all_responses["involvement_fest"] = await typeformAPI.responses.list({
+          uid:'mxuplTow',
+          pageSize:1000,
+        });
+    for (const x of all_responses["involvement_fest"]["items"]){
+          all_emails_schools.push([x["answers"][2]["email"], "University of Maryland-Baltimore County", ""])
+      }
+    all_responses["priority_registrations"] = await typeformAPI.responses.list({
+              uid:'Lv81gHpJ',
+              pageSize:1000,
+        });
+      for (const x of all_responses["priority_registrations"]["items"]){
+          all_emails_schools.push([x["answers"][2]["email"], x["answers"][5]["text"], ""])
+      }
+
+    return all_emails_schools;
+
+
+}
+//main_registration2 = typeform.list('Bcr4BHNV',pageSize= 1000, before=main_registration1[-1]["token"])['items']
+//involvement_fest = typeform.list('mxuplTow',pageSize= 1000)['items']
+//priority_registrations = typeform.list('Lv81gHpJ',pageSize= 1000)['items']
+
 const registeredList = new Discord.Collection();
 
 bot.on("ready", async () => {
     console.log("I'm ready");
-    
-    /* for(const id of IDList)
-        registeredList.set(id, []); */
-
+    responses = await getAPIResponses();
+    console.log(responses)
     readList();
-
-    bot.user.setActivity("Thanks for tuning in!", { type: "COMPETING" });
+    bot.user.setActivity("", { type: "COMPETING" });
 
     // console.log(registeredList.get("FCXWU"));
 });
@@ -33,12 +88,16 @@ bot.on("message", async msg => {
 
     if(msg.channel.type == "dm")
     {
+        if (msg.content.includes("@")){
+          responses = await getAPIResponses();
+          console.log(responses)
+        }
         for(const id of registeredList.keyArray())
         {
             if(msg.content.startsWith(id))
             {
                 const authorID = msg.author.id;
-                
+
                 if(!registeredList.get(id).includes(`<@!${authorID}>`))
                 {
                     registeredList.get(id).push(`<@!${authorID}>`);
@@ -46,19 +105,19 @@ bot.on("message", async msg => {
                 }
                 else
                     msg.channel.send("You've already registered for this event!");
-            }            
+            }
         }
 
         console.log(`${msg.author.username} said "${msg.content}"`);
-        return;        
+        return;
     }
     const prefixRegex = new RegExp(`^(<@!?${bot.user.id}>)\\s*`);
 	if (!prefixRegex.test(msg.content)) return;
-    
+
 	const [, matchedPrefix] = msg.content.match(prefixRegex);
 	const args = msg.content.slice(matchedPrefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
-    
+
     if(msg.member.roles.cache.has(settings.COMMAND_ROLE))
     {
         if(command == "list")
@@ -69,13 +128,13 @@ bot.on("message", async msg => {
                 output.push(`${key}:    ${val.length}\n`);
             });
             output.push(`\`\`\``);
-            
+
             msg.channel.send("Here's the list: ");
             msg.channel.send(output.join(""));
         }
         else if(command == "add")
         {
-            // Add a new ID to the list of current IDs 
+            // Add a new ID to the list of current IDs
             const newID = args[0];
 
             if(registeredList.has(newID))
@@ -88,7 +147,7 @@ bot.on("message", async msg => {
         }
         else if(command == "remove")
         {
-            // Add a new ID to the list of current IDs 
+            // Add a new ID to the list of current IDs
             const IDtoRemove = args[0];
 
             if(registeredList.has(IDtoRemove))
@@ -99,7 +158,7 @@ bot.on("message", async msg => {
                     registeredList.delete(IDtoRemove);
                     msg.channel.send(`"${IDtoRemove}" was removed!`);
                 }
-            
+
             saveList();
         }
         else if(command == "who")
@@ -111,7 +170,7 @@ bot.on("message", async msg => {
                 msg.channel.send(`This ID does not exist?`);
                 return;
             }
-            
+
             const users = registeredList.get(IDToFetch);
 
             /* const resolvedUsers = []
@@ -143,8 +202,8 @@ bot.on("message", async msg => {
             for(const arr of registeredList.values())
                 for(const val of arr)
                     set.add(val);
-            
-            msg.channel.send(`So far, we have **${set.size}** different users who have checked in!`);            
+
+            msg.channel.send(`So far, we have **${set.size}** different users who have checked in!`);
         }
         else if(command == "compile")
         {
@@ -163,7 +222,7 @@ bot.on("message", async msg => {
         {
             const guild = await bot.guilds.fetch("767955561873670154");
             const compiledList = new Set();
-            
+
             for(const arr of registeredList.values())
                 for(const val of arr)
                 {
@@ -171,7 +230,7 @@ bot.on("message", async msg => {
                     if(gmember)
                         compiledList.add(`${gmember.user.tag}`);
                 }
-            
+
             console.log(compiledList.size);
             const newList = Array.from(compiledList).filter(user => names.includes(user));
 
@@ -183,7 +242,7 @@ bot.on("message", async msg => {
         {
             const guild = await bot.guilds.fetch("767955561873670154");
             const compiledList = new Set();
-            
+
             for(const arr of registeredList.values())
                 for(const val of arr)
                 {
@@ -191,7 +250,7 @@ bot.on("message", async msg => {
                     if(gmember)
                         compiledList.add(`${gmember.user.tag}`);
                 }
-            
+
             console.log(compiledList.size);
             const newList = Array.from(compiledList).filter(user => names.includes(user));
 
@@ -199,7 +258,7 @@ bot.on("message", async msg => {
                 if(newList.includes(member.user.tag))
                     member.user.send("Hi there, we are reaching out to you because you qualified to get hackUMBC Swag, please fill out this form by Tuesday 11:59pm EST: https://hackumbc.typeform.com/to/IIUtQNVg\nHappy Hacking! :3");
             });
-            
+
             msg.react(`👍`);
         }
     }
@@ -214,7 +273,7 @@ function saveList()
     const objectMap = {};
     registeredList.keyArray().forEach(key => objectMap[key] = []);
     registeredList.mapValues((v, key) => objectMap[key].push(...v));
-    
+
     fs.writeFileSync("./data/registerListLog.json", JSON.stringify(objectMap));
 }
 
@@ -229,4 +288,3 @@ function readList()
 }
 
 bot.login(process.env.TOKEN);
-
